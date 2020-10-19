d79b57790c3ae30d1ec16eee1dd52c6bdaa35784
yewseng chu <yewseng.chu@gmail.com>
2020-10-19 16:00:52 +0800

    Initial Commit with Launch Screen

---
+++ iGrade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iGrade
+//
+//  Created by Yew Seng Chu on 19/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
